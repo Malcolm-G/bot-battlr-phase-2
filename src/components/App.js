f4504f0c69be8cd2b8c33e9cca4b9fcb634d837c
Malcolm-G <malcolm.githuka@gmail.com>
2023-01-22 13:04:06 +0300

    Createed MyList component structure and installed react-horizontal-scrolling

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import logo from '../logo.svg';
+import '../stylesheets/App.css';
+import MyList from './MyList';
+
+function App() {
+  return (
+    <div className="App container">
+      <MyList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../stylesheets/MyList.css'
+
+function MyList(){
+    return(
+        <div id="my-list" className="fixed-top" >
+
+        </div>
+    )
+}
+
+export default MyList
